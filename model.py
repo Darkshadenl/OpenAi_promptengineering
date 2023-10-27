6d85133d73e5f75eaf8579f97c16c366f14e341b
Quinten M <quinten@infi.nl>
2023-10-27 16:45:19 +0200

    prompting

--- model.py
+++ model.py
@@ -2,15 +2,22 @@ from datetime import datetime
 
 import replicate
 
-user_system_prompt = ''
-user_max_new_tokens = 128
+user_system_prompt = '''
+You are a Typescript professional. You determine if comments are still up to date, and if not, update them 
+while taking the context of the code into account. 
+You don't change the code if it not clear what should be changed.
+You don't return any text like "Here's the improved version". 
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. 
+If you don't know the answer to a question, please don't share false information.
+'''
+user_max_new_tokens = 4000
 user_min_new_tokens = -1
 user_temperature = 0.75
 user_top_p = 0.9
 user_top_k = 50
 user_stop_sequences = None
 user_seed = None
-user_debug = None
+user_debug = True
 
 
 class ModelHandler:
@@ -28,7 +35,8 @@ class ModelHandler:
     def create_prediction(self):
         self.prediction = replicate.predictions.create(
             version=self.version,
-            input=self.input.to_dict()
+            input=self.input.to_dict(),
+
         )
         self.start_time = datetime.now()
 
@@ -50,19 +58,20 @@ class Llama2ChatInput:
 
     def __init__(self, prompt):
         self.prompt = prompt
-        self.user_system_prompt = user_system_prompt
-        self.user_max_new_tokens = user_max_new_tokens
-        self.user_min_new_tokens = user_min_new_tokens
-        self.user_temperature = user_temperature
-        self.user_top_p = user_top_p
-        self.user_top_k = user_top_k
-        self.user_stop_sequences = user_stop_sequences
-        self.user_seed = user_seed
+        self.system_prompt = user_system_prompt
+        self.max_new_tokens = user_max_new_tokens
+        self.min_new_tokens = user_min_new_tokens
+        self.temperature = user_temperature
+        self.top_p = user_top_p
+        self.top_k = user_top_k
+        self.stop_sequences = user_stop_sequences
+        self.seed = user_seed
         self.debug = user_debug
 
     def to_dict(self):
-        return {
+        thedict = {
             key: value
             for key, value in vars(self).items()
             if value is not None
         }
+        return thedict
--- python.py
+++ python.py
@@ -13,29 +13,242 @@ models = [
 
 prompt = """
 [INST]
-Check the comments in the given piece of code. If the comments are not correct, update them.
-Replace the text within the <summary></summary>, <param></param> and <returns></returns> blocks with the newly generated comment
-if one is applicable. 
+The subject of this prompt is within the [CODE][/CODE]  tags.
+Check the comments within the <RETURNABLE> tags in the given piece of [CODE]. 
 Only return the text within <RETURNABLE></RETURNABLE> blocks. 
-Comments are not code. Do not add or change any code. COMMENT ONLY.  
+Comments are not code. Do not add or change any code. COMMENTS ONLY.  
 Do not add any new comments. UPDATE EXISTING COMMENTS ONLY. 
-Do not add any text.[/INST]\n
+EXAMPLE:
+If I send you the following Typescript code: 
+<RETURNABLE> 
+/**
+ * Adds two numbers and returns the result.
+ * @param num1 - The first number to be added.
+ * @param num2 - The second number to be added.
+ * @returns The sum of the two numbers.
+ */
+function addNumbers(num1: number, num2: number): number {
+</RETURNABLE> 
+  return num1 + num2;
+}
+You MUST return it in the following format:
+<NEWCOMMENT>
+/**
+  * Adds two numbers and returns the result.
+  * @param num1 - The first number to be added.
+  * @param num2 - The second number to be added.
+  * @returns The sum of the two numbers.
+*/
+function addNumbers(num1: number, num2: number): number {
+</NEWCOMMENT> 
+If you find multiple <RETURNABLE> blocks, you must return them in the same order as they appear in the code.
+This would look like this:
+<NEWCOMMENT>
+{The updated RETURNABLE block}
+</NEWCOMMENT>
+<NEWCOMMENT>
+{The updated RETURNABLE block}
+</NEWCOMMENT>
+
+
+Now go ahead and process the following code within the [CODE] tags:
+[/INST]
+[CODE]
 ${code}
+[/CODE]
 """
 
 code = """
-<RETURNABLE>
-/// <summary>
-/// Finds the minimum of two integers.
-/// </summary>
-/// <param name="a">The first integer.</param>
-/// <param name="b">The second integer.</param>
-/// <returns>The minimum of the two integers.</returns>
-</RETURNABLE>
-public int FindMax(int a, int b)
-{
-    return (a > b) ? a : b;
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { DateTime } from 'luxon';
+import { Authenticated } from '~/auth/authenticated.decorator';
+import { Hour, HourService } from '~/domain';
+import { CreateInvoiceDto, CreateInvoiceLineDto, InvoiceDto, SimplicateAdapter } from '~/infrastructure/simplicate';
+
+interface CrossInvoiceServiceHours {
+  name: string;
+  totalAmount: number;
+  totalDuration: number;
+  hours: Hour[];
 }
+
+interface CrossInvoiceProjectHours {
+  name: string;
+  totalAmount: number;
+  totalDuration: number;
+  services: CrossInvoiceServiceHours[];
+}
+
+interface CrossInvoiceHours {
+  vendorId: string;
+  vendorName: string;
+  supplierId: string;
+  supplierName: string;
+  supplierMyOrganizationProfileId: string;
+  totalAmount: number;
+  totalDuration: number;
+  projects: CrossInvoiceProjectHours[];
+}
+
+<RETURNABLE>
+/**
+ * This is the MoneyController class which handles API requests related to hours.
+ */
+@Authenticated()
+@Controller('api/hours')
+export class HourController {
+</RETURNABLE>
+  constructor(private readonly service: HourService, private readonly simplicate: SimplicateAdapter) {}
+
+  @Get('date/:start/:end')
+  getRange(@Param('start') start: string, @Param('end') end: string) {
+    const startDate = DateTime.fromFormat(start, 'yyyy-MM-dd');
+    const endDate = DateTime.fromFormat(end, 'yyyy-MM-dd').endOf('day');
+
+    return this.service.find(startDate, endDate);
+  }
+  
+  <RETURNABLE>
+  /**
+   * Retrieves the months data based on the provided week year and week number.
+   *
+   * @param weekYear - The year of the week.
+   * @param weekNumber - The number of the week.
+   * @returns The data for the specified week.
+   */
+  @Get('week/:weekYear/:weekNumber')
+  getWeek(@Param('weekYear') weekYear: number, @Param('weekNumber') weekNumber: number) {
+  </RETURNABLE>
+    const startDate = DateTime.fromObject({ weekYear, weekNumber }).startOf('week');
+    const endDate = startDate.endOf('week');
+    return this.service.find(startDate, endDate);
+  }
+
+  @Get('month/:year/:month')
+  async getMonth(@Param('year') year: number, @Param('month') month: number) {
+    const startDate = DateTime.fromObject({ year, month, day: 1 });
+    const endDate = startDate.endOf('month');
+
+    return this.service.find(startDate, endDate);
+  }
+
+  @Post('cross/invoices/:year/:month')
+  async createInvoices(@Param('year') year: number, @Param('month') month: number): Promise<void> {
+    const invoiceData = await this.collectInvoiceData(year, month);
+    await Promise.all(invoiceData.invoices.map((data) => this.simplicate.postInvoice(data)));
+  }
+
+  @Get('cross/invoices/:year/:month')
+  async collectInvoiceData(
+    @Param('year') year: number,
+    @Param('month') month: number
+  ): Promise<{ invoiced: CrossInvoiceHours[]; uninvoiced: CrossInvoiceHours[]; invoices: InvoiceDto[] }> {
+    const invoiced = await this.getCross(year, month, { invoiced: true });
+    const uninvoiced = await this.getCross(year, month, { uninvoiced: true });
+    const invoices = await Promise.all(
+      invoiced.map(async (cross) => {
+        const my_organization_profile_id = cross.supplierMyOrganizationProfileId;
+        const organization = await this.simplicate.getOrganization(cross.vendorId);
+        const payment_term_id = organization.debtor?.payment_term.id;
+        const vat_class = await this.simplicate.getVatClass('0%');
+        const status = await this.simplicate.getInvoiceStatus('Draft');
+        const revenue_group = await this.simplicate.getRevenueGroup('Algemeen');
+        const invoice_lines: CreateInvoiceLineDto[] = cross.projects.flatMap((project) =>
+          project.services.map((service) => ({
+            date: DateTime.now().toFormat('yyyy-MM-dd'),
+            description: project.name + ': ' + service.name,
+            amount: service.totalDuration,
+            price: service.totalAmount / service.totalDuration,
+            vat_class_id: vat_class.id,
+            revenue_group_id: revenue_group.id,
+          }))
+        );
+        <RETURNABLE>
+        const organization_id = cross.vendorId; // replace man.vendorId with cross.vendorId at a later point 
+        </RETURNABLE>
+        const status_id = status.id;
+        const invoice: CreateInvoiceDto = {
+          date: DateTime.now().toFormat('yyyy-MM-dd'),
+          subject: `Werkzaamheden van ${cross.supplierName} voor ${cross.vendorName} in ${year}-${month}`,
+          reference: `INF-CROSS-${year}-${month}`,
+          my_organization_profile_id,
+          organization_id,
+          payment_term_id,
+          invoice_lines,
+          status_id,
+          sending_method: 'email',
+        };
+        return invoice;
+      })
+    );
+    return { invoiced, uninvoiced, invoices };
+  }
+
+  @Get('cross/:year/:month')
+  async getCross(
+    @Param('year') year: number,
+    @Param('month') month: number,
+    @Query() query: { invoiced?: unknown; uninvoiced?: unknown }
+  ): Promise<CrossInvoiceHours[]> {
+    const startDate = DateTime.fromObject({ year, month, day: 1 });
+    const endDate = startDate.endOf('month');
+    const showInvoicedOnly = 'invoiced' in query;
+    const showUninvoicedOnly = 'uninvoiced' in query;
+    const hours = await this.service.find(startDate, endDate);
+    return hours
+      .filter(
+        (hour) =>
+          hour.vendorId &&
+          hour.vendorId !== hour.supplierId &&
+          (!(showInvoicedOnly || showUninvoicedOnly) ||
+            (showInvoicedOnly && hour.isInvoiced) ||
+            (showUninvoicedOnly && !hour.isInvoiced))
+      )
+      .reduce<CrossInvoiceHours[]>((accu, hour) => {
+        let invoice = accu.find((i) => i.vendorName === hour.vendorName && i.supplierName === hour.supplierName);
+        if (!invoice) {
+          invoice = {
+            vendorId: hour.vendorId,
+            vendorName: hour.vendorName,
+            supplierName: hour.supplierName,
+            supplierId: hour.supplierId,
+            supplierMyOrganizationProfileId: hour.supplierMyOrganizationProfileId,
+            totalAmount: 0,
+            totalDuration: 0,
+            projects: [],
+          };
+          accu.push(invoice);
+        }
+        let project = invoice.projects.find((p) => p.name === hour.customerName + ':' + hour.projectName);
+        if (!project) {
+          project = {
+            name: hour.customerName + ':' + hour.projectName,
+            totalDuration: 0,
+            totalAmount: 0,
+            services: [],
+          };
+          invoice.projects.push(project);
+        }
+        let service = project.services.find((s) => s.name == hour.serviceName + ' @ ' + hour.rate);
+        if (!service) {
+          service = { name: hour.serviceName + ' @ ' + hour.rate, totalDuration: 0, totalAmount: 0, hours: [] };
+          project.services.push(service);
+        }
+
+        const { durationHours, rate } = hour;
+        invoice.totalAmount += durationHours * rate;
+        invoice.totalDuration += durationHours;
+        project.totalAmount += durationHours * rate;
+        project.totalDuration += durationHours;
+        service.totalAmount += durationHours * rate;
+        service.totalDuration += durationHours;
+        service.hours.push(hour);
+
+        return accu;
+      }, []);
+  }
+}
+
 """
 prompt = prompt.replace('${code}', code)
 
