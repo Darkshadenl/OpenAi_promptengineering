0524553a8be6ee80cb052af5bd8c2bbeb80f2f5d
Quinten M <quinten@infi.nl>
2023-10-31 16:30:16 +0100

    save correct or not

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 
-def save_to_db(start_time, end_time, prompt, system_prompt, message_content):
+def save_to_db(start_time, end_time, prompt, system_prompt, message_content, correct=False):
     conn = sqlite3.connect('openai_model_handler.db')
     cursor = conn.cursor()
     cursor.execute('''
-    INSERT INTO requests (start_time, end_time, prompt, system_prompt, message_content)
-    VALUES (?, ?, ?, ?, ?);
-    ''', (start_time, end_time, prompt, system_prompt, message_content))
+    INSERT INTO requests (start_time, end_time, prompt, system_prompt, message_content, correct)
+    VALUES (?, ?, ?, ?, ?, ?);
+    ''', (start_time, end_time, prompt, system_prompt, message_content, correct))
     conn.commit()
     conn.close()
 
@@ -25,5 +25,9 @@ def setup_db():
         message_content TEXT
     );
     ''')
+    cursor.execute('''
+        ALTER TABLE requests ADD COLUMN correct BOOLEAN;
+        ''')
     conn.commit()
     conn.close()
+
--- python.py
+++ python.py
@@ -10,7 +10,6 @@ load_dotenv()
 
 system_prompt = '''
 You are a Typescript professional. 
-You are not allowed to change or update any implementation code (code that is not a comment).
 You don't return any additional text or explanations like "Here's the improved version".
 You give no return if you don't understand the given prompt. 
 If you don't know the answer to a question, please don't share false information.
@@ -22,49 +21,45 @@ The task is to update existing code comments based on the corresponding code.
 We assume that the code is correct and always the source of truth. 
 If there is a discrepancy between the code and the comment, the comment is considered incorrect and needs to be updated 
 to align with the code.
-Only return what is placed between these tags: <RETURNABLE></RETURNABLE>.
-Do not return the tags themselves.   
 If this piece of code is given:
-<RETURNABLE> 
 /// <summary>
 /// Checks if a string contains only alphabets.
 /// </summary>
 /// <param name="str">The string to check</param>
 /// <returns>True if the string is a palindrome, else False</returns>
 public bool IsPalindrome(string str)
-</RETURNABLE> 
 {
     string reversed = new string(str.Reverse().ToArray());
     return str.Equals(reversed, StringComparison.OrdinalIgnoreCase);
 }
 
 
-You return this:
+You place returnable tags and only return the comment and signature of the class or function:
+<RETURNABLE>
 /// <summary>
 /// Checks if a string is a palindrome.
 /// </summary>
 /// <param name="str">The string to check</param>
 /// <returns>True if the string is a palindrome, else False</returns>
 public bool IsPalindrome(string str)
+</RETURNABLE>
 
 Here's the code that you have to correct:
-```typescript 
 ${code} 
-```
 """
 code = """
-<RETURNABLE> 
 /// <summary>
-/// Splits a string by space.
+/// Converts a string to uppercase.
 /// </summary>
-/// <param name="a">The first string</param>
-/// <param name="b">The second string</param>s
-/// <returns>The concatenated string</returns>
-public string ConcatStrings(string a, string b)
-</RETURNABLE> 
+/// <param name="input">The string to reverse</param>
+/// <returns>The reversed string</returns>
+public string ReverseString(string input)
 {
-    return a + b;
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
 }
+
 """
 prompt = prompt.replace('${code}', code)
 
@@ -93,10 +88,15 @@ def main():
     handler.create_prediction()
     end_time = datetime.now()
 
-    print("Done!")
     message = handler.completion.choices[0].message
-    print(message.content)
-    save_to_db(start_time, end_time, prompt, system_prompt, message.content)
+    print(message.content + "\n")
+    user_correct = input("Correct? y/n")
+    correct = False
+
+    if user_correct == 'y':
+        correct = True
+
+    save_to_db(start_time, end_time, prompt, system_prompt, message.content, correct)
 
 
 if __name__ == '__main__':
